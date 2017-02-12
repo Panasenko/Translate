357b5abb658b73d1088449f55247d8ba7597ebac
Panasenko <panasenkomaksim@gmail.com>
2017-02-12 12:18:50 +0200

    add translator back

--- translator/js/injection.js
+++ translator/js/injection.js
@@ -7,39 +7,41 @@
             var modal = document.getElementsByClassName('modal-footer');
             modal[0].innerHTML =  "<button id=\"translater\" type=\"button\" class=\"btn btn-info\">Перевести</button>" + modal[0].innerHTML;
            //Устанавливаем обработчик на кнопку Перевести
-        }, 100)
-            var master = "ru";
             document.getElementById('translater').addEventListener("click", function () {
-                //Получаем значение поля Ввода информации
-
-                console.log(master)
-                if(document.getElementById('create-info').value.length > 0){
-                   var arch = document.getElementById('create-info').value
-                } else {
-                    alert("В поле описания ИП пусто")
-                }
-
-                if(master === "ru"){
-
-                    requestTrans(arch).then(function(response) {
-                        return JSON.parse(response).trans
-                    }).then(function(response) {
-                            backResult(response)
-                        })
-
-                    master = "uk";
-
-                } else if(master === "uk"){
-                    backResult(arch)
-                    master = "ru";
-                } else {
-                    alert("Ошибка выбора языка")
-                }
-                console.log(master)
-
+                main(localStorage["master"] || "ru")
             });
 
+           // document.getElementsByClassName('close').addEventListener("click", clienlocalStorage);
+            document.getElementById('apply-create-problem').addEventListener("click", clienlocalStorage);
+            document.querySelectorAll('.modal-footer')[0].children[1].addEventListener("click", clienlocalStorage);
+            document.querySelectorAll('.modal-footer')[0].children[2].addEventListener("click", clienlocalStorage);
+
+
+        }, 100)
     });
+
+        function main(master) {
+            if (master === "ru") {
+                localStorage["translit"] = document.getElementById('create-info').value;
+                requestTrans(document.getElementById('create-info').value).then(function (response) {
+                    return JSON.parse(response).trans
+                }).then(function (response) {
+                    localStorage["master"] = "uk";
+                    backResult(response)
+                });
+            } else if (master === "uk") {
+                if(localStorage["translit"]){
+                    localStorage["master"] = "ru";
+                    backResult(localStorage["translit"]);
+                } else {
+                    alert("Архив не найден в localStorage")
+                }
+            } else {
+                alert("Ошибка выбора языка")
+            }
+        }
+
+
 
 
     function requestTrans(text){
@@ -55,7 +57,6 @@
                     reject(Error(req.statusText));
                 }
             };
-
             req.onerror = function() {
                 reject(Error("Network Error"));
             };
@@ -65,6 +66,12 @@
 
     function backResult(text){
         document.getElementById('create-info').value = text;
+    }
+
+    //удаляет все элементы из localStorage
+    function clienlocalStorage(){
+        delete localStorage["translit"];
+        delete localStorage["master"];
     }
 
 
